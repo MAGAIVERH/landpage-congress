b13189fd07960b388316f66c3c5a7fa10475f4b2
Magaiver <magaivermagalhaes.mm@gmail.com>
2024-06-18 11:09:07 -0300

    fix: border-radius on the button

--- src/app/components/Hero-Section.tsx
+++ src/app/components/Hero-Section.tsx
@@ -99,7 +99,7 @@ const HeroSection = () => {
         </div>
         <a
           href="#register"
-          className="bg-green-500 hover:bg-green-700 text-white font-bold py-3 px-14 md:py-4 md:px-8 rounded-[20px] inline-block mt-4"
+          className="bg-green-500 hover:bg-green-700 text-white font-bold py-3 px-14 md:py-4 md:px-8 rounded-[15px] border-none inline-block mt-4"
         >
           Inscreva-se Agora
         </a>

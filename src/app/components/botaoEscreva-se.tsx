537fe0fc0d286853fd32dbdb3d9917f45ca9e3ec
Magaiver <magaivermagalhaes.mm@gmail.com>
2024-06-18 22:26:44 -0300

    feat: Created button component

---
+++ src/app/components/botaoEscreva-se.tsx
@@ -0,0 +1,10 @@
+const BotaoEscreva = () => {
+    return (   <a
+        href="#register"
+        className="bg-green-500 hover:bg-green-700 text-white font-bold py-3 px-14 md:py-4 md:px-8 rounded-[15px] border-none inline-block mt-4"
+      >
+        Inscreva-se Agora
+      </a> );
+}
+ 
+export default BotaoEscreva;

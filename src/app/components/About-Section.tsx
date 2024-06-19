31d40219bfe9c324288151dc07c3e598b350b24b
Magaiver <magaivermagalhaes.mm@gmail.com>
2024-06-18 22:25:56 -0300

    feat: Created three sub-components for AboutSection and Added course information cards components

--- src/app/components/About-Section.tsx
+++ src/app/components/About-Section.tsx
@@ -1,19 +1,34 @@
+import FirstAbout from "./about/FirstAbout";
+import SecondAbout from "./about/SecondAbout";
+import ThirdAbout from "./about/ThirdAbout";
+
 const AboutSection = () => {
   return (
     <section id="about" className="py-10 bg-gray-100">
       <div className="container mx-auto text-center p-4">
-        <h2 className="text-2xl md:text-3xl font-bold mb-6">
-          Sobre o Congresso
+        <h2 className="text-4xl md:text-3xl font-bold mb-10 text-[#ea0bb4]">
+          Inscreva-se
         </h2>
-        <p className="text-md md:text-lg">
-          Nosso congresso pediátrico oferece uma oportunidade única para médicos
-          e estudantes se atualizarem com as últimas pesquisas e práticas na
-          área da pediatria. Participe e tenha acesso a palestras, workshops e
-          muito mais.
-        </p>
+   
+        <FirstAbout />
+        
+        <h2 className="text-4xl md:text-3xl font-bold mb-10 text-[#ea0bb4] mt-10">
+          XLIII Jornada de Ginecologia e Obstetrícia e VII Jornada Iguatuense de Ginecologia e Obstetrícia.
+        </h2>
+
+        <SecondAbout />
+
+        <h2 className="text-4xl md:text-3xl font-bold mb-10 text-[#ea0bb4] mt-10">
+          INSCREVA-SE NOS CURSOS DE ATUALIZAÇÃO
+        </h2>
+        <p className='text-[red] mb-8'>Aviso: Para se inscrever nos cursos é necessário estar inscrito na Jornada</p>
+
+        <ThirdAbout />
       </div>
     </section>
   );
 };
 
 export default AboutSection;
+
+
---
+++ src/app/components/about/SecondAbout.tsx
@@ -0,0 +1,21 @@
+import BotaoEscreva from '../botaoEscreva-se';
+import infoCursos from '../data/infoCursosaData';
+
+const SecondAbout = () => {
+    return (
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 ">
+        {infoCursos.slice(3, 9).map((curso, index) => (
+          <div key={index} className="bg-white p-6 rounded-lg shadow-lg">
+            <h3 className="text-xl font-bold text-[black] mb-2">{curso.titulo}</h3>
+            {curso.oferta && <p className="text-green-600 font-semibold">{curso.oferta}</p>}
+            {curso.status && <p className="text-yellow-600">{curso.status}</p>}
+            {curso.preco && <p className="text-red-600">{curso.preco}</p>}
+            <p className="text-gray-600">{curso.data}</p>
+            <BotaoEscreva/>
+          </div>
+        ))}
+      </div>
+    );
+  };
+
+  export default SecondAbout;

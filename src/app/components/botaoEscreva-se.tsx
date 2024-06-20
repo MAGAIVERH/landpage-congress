398e6f572cf5b5e5dd7e55978d4ffe5ba7fa4af9
Magaiver <magaivermagalhaes.mm@gmail.com>
2024-06-20 13:20:12 -0300

    feat: Add className prop to button component

--- src/app/components/botaoEscreva-se.tsx
+++ src/app/components/botaoEscreva-se.tsx
@@ -1,5 +1,4 @@
-'use client'
-
+'use client';
 import React from 'react';
 
 interface BotaoEscrevaProps {
@@ -7,17 +6,25 @@ interface BotaoEscrevaProps {
   bgColor: string;
   hoverColor: string;
   onClick?: () => void;
+  className?: string;
 }
 
-const BotaoEscreva: React.FC<BotaoEscrevaProps> = ({ text, bgColor, hoverColor, onClick }) => {
+const BotaoEscreva: React.FC<BotaoEscrevaProps> = ({
+  text,
+  bgColor,
+  hoverColor,
+  onClick,
+  className,
+}) => {
   return (
     <button
-      className={`text-white font-bold text-lg py-3 px-20 md:py-4 md:px-24 rounded-[15px] border-none inline-block mt-4`}
+      className={`text-white font-bold text-lg py-3 px-20 md:py-4 md:px-24 rounded-[15px] border-none inline-block mt-4 ${className}`}
       style={{
         backgroundColor: bgColor,
       }}
       onMouseEnter={(e) => {
-        (e.currentTarget as HTMLButtonElement).style.backgroundColor = hoverColor;
+        (e.currentTarget as HTMLButtonElement).style.backgroundColor =
+          hoverColor;
       }}
       onMouseLeave={(e) => {
         (e.currentTarget as HTMLButtonElement).style.backgroundColor = bgColor;
@@ -30,4 +37,3 @@ const BotaoEscreva: React.FC<BotaoEscrevaProps> = ({ text, bgColor, hoverColor, 
 };
 
 export default BotaoEscreva;
-

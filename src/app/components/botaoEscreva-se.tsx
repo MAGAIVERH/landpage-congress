744d7571d897e372238d932857e051c9d73c4509
Magaiver <magaivermagalhaes.mm@gmail.com>
2024-06-30 14:44:10 -0300

    feat: add redirectTo

--- src/app/components/botaoEscreva-se.tsx
+++ src/app/components/botaoEscreva-se.tsx
@@ -1,21 +1,35 @@
 'use client';
 import React from 'react';
+import { useRouter } from 'next/navigation';
 
 interface BotaoEscrevaProps {
   text: string;
   bgColor: string;
   hoverColor: string;
-  onClick?: () => void;
+  redirectTo?: string;
   className?: string;
+  onClick?: () => void;
 }
 
 const BotaoEscreva: React.FC<BotaoEscrevaProps> = ({
   text,
   bgColor,
   hoverColor,
-  onClick,
+  redirectTo,
   className,
+  onClick,
 }) => {
+  const router = useRouter();
+
+  const handleClick = () => {
+    if (redirectTo) {
+      router.push(redirectTo); // Redirecionar diretamente
+    }
+    if (onClick) {
+      onClick(); // Chama onClick se estiver definido
+    }
+  };
+
   return (
     <button
       className={`text-white font-bold text-lg py-3 px-20 md:py-4 md:px-24 rounded-[15px] border-none inline-block mt-4 ${className}`}
@@ -29,7 +43,7 @@ const BotaoEscreva: React.FC<BotaoEscrevaProps> = ({
       onMouseLeave={(e) => {
         (e.currentTarget as HTMLButtonElement).style.backgroundColor = bgColor;
       }}
-      onClick={onClick}
+      onClick={handleClick}
     >
       {text}
     </button>
